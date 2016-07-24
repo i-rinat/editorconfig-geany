1fde1e8aa736d7dcb1dac9e653cbc1de08a364b9
Rinat Ibragimov <ibragimovrinat@mail.ru>
2016-07-24 15:56:50 +0300

    fix comment style, remove stale comments

--- editorconfig-geany.c
+++ editorconfig-geany.c
@@ -23,28 +23,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* indent_size = -1000 means indent_size = tab*/
+// indent_size = -1000 means indent_size = tab
 #define INDENT_SIZE_TAB (-1000)
 
-//~ static GeanyPlugin *geany_plugin;
-//~ static GeanyData *geany_data;
-//~ static GeanyFunctions *geany_functions;
-
-/* Reload EditorConfig menu item */
 static GtkWidget *menu_item_reload_editorconfig;
-
-//~ static void
-//~ on_document_open(GObject *obj, GeanyDocument *gd, gpointer user_data);
-
-//~ static void
-//~ on_geany_startup_complete(GObject *obj, gpointer user_data);
-
-//~ /* plugin signals */
-//~ PluginCallback plugin_callbacks2[] = {
-    //~ {"document-open", (GCallback)&on_document_open, TRUE, NULL},
-    //~ {"geany-startup-complete", (GCallback)&on_geany_startup_complete, TRUE,
-     //~ NULL},
-    //~ {NULL, NULL, FALSE, NULL}};
 
 static int
 load_editorconfig(const GeanyDocument *gd)
@@ -54,7 +36,7 @@ load_editorconfig(const GeanyDocument *gd)
         int indent_size;
         int tab_width;
         const char *end_of_line;
-    } ecConf; /* obtained EditorConfig settings will be here */
+    } ecConf;
 
     int i;
     editorconfig_handle eh = editorconfig_handle_init();
@@ -64,20 +46,19 @@ load_editorconfig(const GeanyDocument *gd)
 
     memset(&ecConf, 0, sizeof(ecConf));
 
-    /* start parsing */
+    // start parsing
     printf("DOC_FILENAME(gd) = %s\n", DOC_FILENAME(gd));
     err_num = editorconfig_parse(DOC_FILENAME(gd), eh);
     if (err_num != 0 && err_num != EDITORCONFIG_PARSE_NOT_FULL_PATH) {
-        /* Ignore full path error, whose error code is
-         * EDITORCONFIG_PARSE_NOT_FULL_PATH */
+        // Ignore full path error, whose error code is
+        // EDITORCONFIG_PARSE_NOT_FULL_PATH
         editorconfig_handle_destroy(eh);
         return err_num;
     }
 
-    /* apply the settings */
+    // apply the settings
     name_value_count = editorconfig_handle_get_name_value_count(eh);
 
-    /* get settings */
     for (i = 0; i < name_value_count; ++i) {
         const char *name;
         const char *value;
@@ -115,10 +96,9 @@ load_editorconfig(const GeanyDocument *gd)
         scintilla_send_message(sci, SCI_SETINDENT, (uptr_t)ecConf.indent_size,
                                0);
 
-        /*
-         * We set the tab width here, so that this could be overrided then
-         * if ecConf.tab_wdith > 0
-         */
+        // We set the tab width here, so that this could be overrided then
+        // if ecConf.tab_wdith > 0
+
         scintilla_send_message(sci, SCI_SETTABWIDTH, (uptr_t)ecConf.indent_size,
                                0);
     }
@@ -130,11 +110,11 @@ load_editorconfig(const GeanyDocument *gd)
     if (ecConf.indent_size == INDENT_SIZE_TAB) {
         int cur_tabwidth = scintilla_send_message(sci, SCI_GETTABWIDTH, 0, 0);
 
-        /* set indent_size to tab_width here */
+        // set indent_size to tab_width here
         scintilla_send_message(sci, SCI_SETINDENT, (uptr_t)cur_tabwidth, 0);
     }
 
-    /* set eol */
+    // set eol
     if (ecConf.end_of_line) {
         if (!strcmp(ecConf.end_of_line, "lf")) {
             scintilla_send_message(sci, SCI_SETEOLMODE, (uptr_t)SC_EOL_LF, 0);
@@ -158,9 +138,6 @@ show_error_message(void)
     dialogs_show_msgbox(GTK_MESSAGE_ERROR, "Failed to reload EditorConfig.");
 }
 
-/*
- * Reload EditorConfig menu call back
- */
 static void
 menu_item_reload_editorconfig_cb(GtkMenuItem *menuitem, gpointer user_data)
 {
@@ -192,7 +169,7 @@ on_geany_startup_complete(GObject *obj, gpointer user_data)
     GeanyData *geany_data = plugin->geany_data; // foreach_document uses it
     int i;
 
-    // load EditorConfig for each GeanyDocument on startup */
+    // load EditorConfig for each GeanyDocument on startup
     foreach_document (i) {
         GeanyDocument *doc = documents[i];
         if (load_editorconfig(doc) != 0)
